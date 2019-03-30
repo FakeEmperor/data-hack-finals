a20fd817674afb1aaaeaddbb4b5925fcc45bf3f7
Alisher Koryakovtsev <alisherk@nvidia.com>
2019-03-30 17:21:24 +0300

    CSV saving

--- save_csv.py
+++ save_csv.py
@@ -1,15 +1,29 @@
-from typing import List
+from typing import List, Union
 from pathlib import Path
-from dno.proto.mock import MockInterop
+from dno.proto.mock import MockInterop, BaseInteropBackend
+from dno.proto.utils import get_project_root
+from loguru import logger
 
 
-def main(tasks: List[str]=None):
+def get_csvs(tasks: List[str]=None, output_dir: Union[Path, str]="data/csvs"):
     """
+    Get csv data
     """
-    Path().glob()
-    tasks = [path.name for path in ]
+    root: Path = get_project_root() / "data" / "besthack19"
+    output_dir = get_project_root() / 'data'/ 'csvs'
+    if tasks is None:
+        tasks = root.glob('*')
+        tasks = [path.name for path in tasks if path.is_file()]
+    if not output_dir.exists():
+        logger.debug("Creating directory structure for the output directory...")
+        output_dir.mkdir(parents=True, exist_ok=True)
 
+    for task in tasks:
+        logger.info(f"Saving info for {task}...")
+        backend = MockInterop(root, task)
+        _, df, _ = backend.get_csv()
+        df.to_csv(str(output_dir / f'{task}.csv'))
 
 
 if __name__ == "__main__":
-    main()
+    get_csvs()
