7abdd2bebbc41ab6006a9aafa710b9f555520ea5
Alisher Koryakovtsev <alisherk@nvidia.com>
2019-03-31 04:23:43 +0300

    Fix: models and result sending

--- dno/model/test.py
+++ dno/model/test.py
@@ -1,24 +1,48 @@
-from dno.model.model import Model
-from dno.proto.mock import TaskReader
-from dno.proto.data import Task
-from dno.proto import utils
+import time
+from typing import Type
 
-if __name__ == "__main__":
-    task_num = 1
+from dno.model.model import Model
+from dno.proto.backend import BackendInteraction
+from dno.proto.mock import TaskReader
+from dno.proto.data import Task, Solution, Map
+from dno.proto import utils
+from loguru import logger
+
+
+def debug_task(model_class: Type[Model], task_num: int):
     task_reader = TaskReader(utils.get_task_path(task_num))
     land_map, tasks, _ = task_reader.read_all()
+    land_map = Map.from_dict(land_map)
+    model = model_class(land_map)
     tasks = list(map(lambda t: Task.from_dict(t['data']), tasks))
-    model = Model(map_raw=land_map)
     mse = 0
     for task in tasks:
         sol = model.handle_task(task)
         if not sol.ready:
             mse += 0.5
         else:
-            err =  (sol.x - task.x) ** 2 + (sol.y - task.y) ** 2
-            err /= len(land_map['map'])
+            err = (sol.x - task.x) ** 2 + (sol.y - task.y) ** 2
+            err /= (land_map.data.shape[0] * land_map.data.shape[1])
             mse += err
     mse /= len(tasks)
-    print(f'Task: {task_num} MSE: {mse}')
+    logger.success(f'Task: {task_num} MSE: {mse}')
 
 
+def run(task_num, model_class: Type[Model]=Model, debug: bool=True):
+    backend = BackendInteraction(backend_host='besthack19.sytes.net', backed_port=4242, auth="exp3ct0pat5onum")
+    if debug:
+        logger.warning(f"RUNNING DEBUGGING FOR TASK {task_num}...")
+        debug_task(model_class, task_num)
+        logger.warning("WAITING SOME TIME FOR YOU TO SEE MUTHAR FACKER")
+        time.sleep(10)
+    logger.warning(f"Starting remote production tests...")
+    land_map = backend.start_task(task_num)
+    model = model_class(land_map)
+    task = backend.send_solution(Solution(ready=False))
+    while not backend.session_ended:
+        task = backend.send_solution(model.handle_task(task))
+    logger.success(f"Final score: {backend.results}")
+
+
+if __name__ == "__main__":
+    run(1)
