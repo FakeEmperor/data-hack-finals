5cdcafb106ef3b4ffd47675ab2acd39cf1eb5478
Alisher Koryakovtsev <alisherk@nvidia.com>
2019-03-31 03:14:03 +0300

    Fix: mock merges

--- dno/proto/mock.py
+++ dno/proto/mock.py
@@ -22,19 +22,11 @@ class MockInterop(BaseInteropBackend):
     def current_iteration(self) -> int:
         return len(self._solutions)
 
-<<<<<<< HEAD
     def __init__(self, base_dir: Union[Path, str]):
         self.base_dir = Path(base_dir)
 
         self._map, self._tasks, self._score = None, None, None
         self._task = None
-=======
-    def __init__(self, base_dir: Union[Path, str], task_name: str):
-        self.base_dir = Path(base_dir)
-        self.task = task_name
-        self._reader = TaskReader(base_dir / task_name)
-        self._map, self._tasks, self._score = self._reader.read_all()
->>>>>>> 2650d543c790d66a33f3d51864b81b247b9dc65e
         self._solutions: List[dict] = []
         self._actual_score: Results = None
 
@@ -83,55 +75,3 @@ class MockInterop(BaseInteropBackend):
             return self._actual_score
         else:
             return Task.from_dict(self._tasks[len(self._solutions)]['data'])
-<<<<<<< HEAD
-=======
-
-
-class TaskReader:
-    """
-    Govno dlya chteniya govna
-    """
-
-    def __init__(self, task_path: Union[str, Path]):
-        self.task_path = Path(task_path)
-        if not self.task_path.exists():
-            raise FileNotFoundError(f"Error: can't find file \"{task_path}\"")
-        self._io_wrapper: BinaryIO = None
-
-    def __enter__(self):
-        self._io_wrapper = open(self.task_path, 'rb')
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._io_wrapper.close()
-
-    def read_all(self) -> Tuple[dict, Sequence[dict], dict]:
-        with self:
-            land_map = self.read_next_response(self._io_wrapper)
-            tasks = []
-            while True:
-                next_size = self._io_wrapper.read(4)
-                if len(next_size) < 4:
-                    break
-                tasks.append(self.read_next_response(self._io_wrapper, next_size))
-
-        return land_map, tasks[:-1], tasks[-1]
-
-    @staticmethod
-    def to_int(data: Union[bytes, str]):
-        data = bytes(data)
-        if len(data) != 4:
-            raise ValueError("Data should be 4 bytes long")
-        return int.from_bytes(data, byteorder="little")
-
-    @staticmethod
-    def read_next_response(stream: IO, packet_size: bytes=None) -> dict:
-        size = TaskReader.to_int(packet_size or stream.read(4))
-        return json.loads(stream.read(size))
-
-
-if __name__ == "__main__":
-    test = TaskReader(r"E:\Source\Repos\ARGUS\PROJECTS\BEST.Hack\Finals\data\besthack19\task1")
-    tests = test.read_all()
-    logger.debug(tests)
->>>>>>> 2650d543c790d66a33f3d51864b81b247b9dc65e
